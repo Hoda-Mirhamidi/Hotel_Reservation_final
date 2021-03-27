a9978168cae55643c8a765997cc2dd7af95d2d41
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-27 20:17:36 +0430

    CapacityFilter updated with a regex to check for the entered capacities correctness [0-4]

--- src/main/java/com/hotel/Hotel_Reservation_final/controller/filters/CapacityFilter.java
+++ src/main/java/com/hotel/Hotel_Reservation_final/controller/filters/CapacityFilter.java
@@ -3,6 +3,9 @@ package com.hotel.Hotel_Reservation_final.controller.filters;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebFilter(filterName = "CapacityFilter")
 public class CapacityFilter implements Filter {
@@ -10,7 +13,21 @@ public class CapacityFilter implements Filter {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(req, resp);
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        String capacity = req.getParameter("capacity");
+        String regex ="^[1-4]$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(capacity);
+        if(matcher.matches()){
+            chain.doFilter(req, resp);
+        }
+        else{
+            out.println("Capacity input is invalid !");
+        }
+        out.println("</body></html>");
     }
 
     public void init(FilterConfig config) throws ServletException {
