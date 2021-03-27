a2cab0341b54675f7a6a5b14b07e63aee8ac0138
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-27 20:10:29 +0430

    MainServlet doPost method updated to redirect the user request to the specific corresponding html file

--- src/main/java/com/hotel/Hotel_Reservation_final/controller/MainServlet.java
+++ src/main/java/com/hotel/Hotel_Reservation_final/controller/MainServlet.java
@@ -6,11 +6,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "MainServlet")
 public class MainServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html");
+        //PrintWriter out = response.getWriter();
+        String option = request.getParameter("options");
+        if(option.equals("reserve")){
+            request.getRequestDispatcher("/reservation.html").include(request,response);
+        }
+        else if(option.equals("modify")){
+            request.getRequestDispatcher("/modification.html");
+        }
+        else if(option.equals("view")){
+            request.getRequestDispatcher("/view.html");
+        }
+        else{
+            request.getRequestDispatcher("/cancel.html");
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
