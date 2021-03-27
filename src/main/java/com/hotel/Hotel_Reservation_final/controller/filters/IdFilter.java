9cf3363959bb8881a8000469ce088ca4c7feaac8
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-27 20:26:21 +0430

    IdFilter updated to check for the length of the id using a regex

--- src/main/java/com/hotel/Hotel_Reservation_final/controller/filters/IdFilter.java
+++ src/main/java/com/hotel/Hotel_Reservation_final/controller/filters/IdFilter.java
@@ -3,6 +3,9 @@ package com.hotel.Hotel_Reservation_final.controller.filters;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebFilter(filterName = "IdFilter")
 public class IdFilter implements Filter {
@@ -10,7 +13,20 @@ public class IdFilter implements Filter {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(req, resp);
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        String id = req.getParameter("id");
+        String regex ="\\d{10}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(id);
+        out.println("<html><body>");
+        if(matcher.matches()){
+            chain.doFilter(req, resp);
+        }
+        else{
+            out.println("Id input is invalid !");
+        }
+        out.println("</body></html>");
     }
 
     public void init(FilterConfig config) throws ServletException {
