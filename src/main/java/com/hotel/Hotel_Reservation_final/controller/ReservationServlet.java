061aa320f0c6f0ac1bbf52af99961ce776b12448
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-27 20:45:05 +0430

    ReservationServlet doPost method updated inorder to create a record in the database with the user data by calling the dao class method addRecord

--- src/main/java/com/hotel/Hotel_Reservation_final/controller/ReservationServlet.java
+++ src/main/java/com/hotel/Hotel_Reservation_final/controller/ReservationServlet.java
@@ -1,16 +1,29 @@
 package com.hotel.Hotel_Reservation_final.controller;
 
+import com.hotel.Hotel_Reservation_final.dao.RoomReservationDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ReservationServlet")
 public class ReservationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String fname = request.getParameter("fname");
+        String lname = request.getParameter("lname");
+        int id = Integer.parseInt(request.getParameter("id"));
+        String start = request.getParameter("first");
+        String end = request.getParameter("end");
+        int capacity = Integer.parseInt(request.getParameter("capacity"));
+        out.println("<html><body>");
+        RoomReservationDao.addRecord(id,fname,lname,start,end,capacity);
+        out.println("</body></html>");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
