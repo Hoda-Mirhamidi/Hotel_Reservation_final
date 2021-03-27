2e871bf1e4e6383500f4a0a31cc3e3d9ec1ce8f9
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-27 21:31:03 +0430

    method for viewing all reservations made by a certain id created

--- src/main/java/com/hotel/Hotel_Reservation_final/dao/RoomReservationDao.java
+++ src/main/java/com/hotel/Hotel_Reservation_final/dao/RoomReservationDao.java
@@ -9,9 +9,10 @@ import java.sql.SQLException;
 
 public class RoomReservationDao {
 
-    private static String insertQuery = "INSERT INTO reservations (customer_id,customer_fname,customer_lname,start_date,end_date,capacity,code) VALUES (?,?,?,?,?,?,?)";
+    private static String insertQuery = "INSERT INTO reservations (customer_id,customer_fname,customer_lname,start_date,end_date,capacity,reservation_code) VALUES (?,?,?,?,?,?,?)";
     private static String getAllReservationsQuery = "SELECT * WHERE customer_id = ? ";
     private static String getRoomQuery = "SELECT room WHERE reservation_code = ? ";
+
     private static Connection connection = DBConnection.INSTANCE.getConnection();
     private static int first_code=10100;
 
@@ -32,11 +33,31 @@ public class RoomReservationDao {
             getRoomNumber.setString(1, String.valueOf(first_code));
             ResultSet rs = getRoomNumber.executeQuery();
             int room = rs.getInt("room");
-            System.out.println("Reservation code : "+first_code+"\nRoom number : "+room);
+            System.out.println("Reservation was successful ! ");
+            System.out.println("Reservation code : "+first_code+" , Room number : "+room);
             first_code++;
         } catch (SQLException throwables) {
             throwables.printStackTrace();
             System.out.println("An error occurred ! Please try again ...");
         }
-}
+    }
+
+    public static void viewAllRecords(int customer_id){
+        try {
+            PreparedStatement getAllReservations = connection.prepareStatement(getAllReservationsQuery);
+            getAllReservations.setInt(1,customer_id);
+            ResultSet rs = getAllReservations.executeQuery();
+            System.out.println("Customer First Name : "+rs.getString("customer_fname")+" , Customer Last Name : "+rs.getString("customer_lname"));
+            while (rs.next()){
+                System.out.println("\nRESERVATION CODE : "+rs.getString("reservation_code"));
+                System.out.println("Starting date : "+rs.getString("start_date"));
+                System.out.println("Ending date : "+rs.getString("end_date"));
+                System.out.println("Room number : "+rs.getString("room"));
+                System.out.println("Capacity : "+rs.getString("capacity"));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 }
