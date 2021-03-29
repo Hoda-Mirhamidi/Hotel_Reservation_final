36f73edb136f786ace122ba593102afa1bd40603
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-30 02:18:09 +0430

    code filter updated

--- src/main/java/com/hotel/Hotel_Reservation_final/controller/filters/CodeFilter.java
+++ src/main/java/com/hotel/Hotel_Reservation_final/controller/filters/CodeFilter.java
@@ -3,6 +3,9 @@ package com.hotel.Hotel_Reservation_final.controller.filters;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebFilter(filterName = "CodeFilter")
 public class CodeFilter implements Filter {
@@ -10,6 +13,20 @@ public class CodeFilter implements Filter {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        String id = req.getParameter("id");
+        String regex ="\\d{5}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(id);
+        out.println("<html><body>");
+        if(matcher.matches()){
+            chain.doFilter(req, resp);
+        }
+        else{
+            out.println("Reservation code is invalid !");
+        }
+        out.println("</body></html>");
         chain.doFilter(req, resp);
     }
 
