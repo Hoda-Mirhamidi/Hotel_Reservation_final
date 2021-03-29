071636586e1e955fe3ba2b6b4643f49c4de90564
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-30 02:49:17 +0430

    ViewServlet updated

--- src/main/java/com/hotel/Hotel_Reservation_final/controller/ViewServlet.java
+++ src/main/java/com/hotel/Hotel_Reservation_final/controller/ViewServlet.java
@@ -1,16 +1,46 @@
 package com.hotel.Hotel_Reservation_final.controller;
 
+import com.hotel.Hotel_Reservation_final.dao.RoomReservationDao;
+import com.hotel.Hotel_Reservation_final.model.RoomReservation;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
 
 @WebServlet(name = "ViewServlet")
 public class ViewServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String id = request.getParameter("id");
+        String code = request.getParameter("code");
+        out.println("<html><body>");
+        if(id.isEmpty() && code.isEmpty()){
+            out.println("both fields cannot be empty !");
+        }
+        else if(id.isEmpty()){
+            RoomReservation reservation = RoomReservationDao.showAllInfo(code);
+            out.println(reservation.toString());
+        }
+        else if(code.isEmpty()){
+            LinkedList<RoomReservation> reservations = RoomReservationDao.viewAllRecords(Integer.parseInt(id));
+            if(reservations != null){
+                reservations.stream().map(RoomReservation::toString).forEach(out::println);
+            }
+            else{
+                out.println("No reservations found ! ");
+            }
+        }else{
+            RoomReservation reservation = RoomReservationDao.showAllInfo(code);
+            assert reservation != null;
+            out.println(reservation.toString());
+        }
+        out.println("</body></html>");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
