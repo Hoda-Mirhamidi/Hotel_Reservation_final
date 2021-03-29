29cfc168c3b420ecfc21ffb8ef601abd3776def7
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-30 02:14:19 +0430

    code filter added to check the correctness of a given reservation code

---
+++ src/main/java/com/hotel/Hotel_Reservation_final/controller/filters/CodeFilter.java
@@ -0,0 +1,20 @@
+package com.hotel.Hotel_Reservation_final.controller.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "CodeFilter")
+public class CodeFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
