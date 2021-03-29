80528fbd0ec3c05086cace71de5bd0de851c8408
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-29 22:40:25 +0430

    ReservationServlet updated to support both making and modifying reservations

--- src/main/java/com/hotel/Hotel_Reservation_final/controller/ReservationServlet.java
+++ src/main/java/com/hotel/Hotel_Reservation_final/controller/ReservationServlet.java
@@ -23,15 +23,33 @@ public class ReservationServlet extends HttpServlet {
         String end = request.getParameter("end");
         int capacity = Integer.parseInt(request.getParameter("capacity"));
         out.println("<html><body>");
-        try{
-            RoomReservation reservation = RoomReservationDao.addRecord(id,fname,lname,start,end,capacity);
-            if(reservation != null){
-                out.println("Reservation was successful ! ");
-                out.println("Reservation code : "+reservation.getReservation_code()+" , Room number : "+reservation.getRoom());
+        String option = request.getParameter("options");
+        if(option.equals("reserve")){
+            try{
+                RoomReservation reservation = RoomReservationDao.addRecord(id,fname,lname,start,end,capacity);
+                if(reservation != null){
+                    out.println("Reservation was successful ! ");
+                    out.println("Reservation code : "+reservation.getReservation_code()+" , Room number : "+reservation.getRoom());
+                }
+            }catch (NullPointerException exception){
+                out.println("An error occurred ! Please try again ...");
+                request.getRequestDispatcher("reservation.html").include(request,response);
             }
-        }catch (NullPointerException exception){
-            out.println("An error occurred ! Please try again ...");
-            request.getRequestDispatcher("reservation.html").include(request,response);
+        }
+        else{
+            String code = request.getParameter("code");
+            try{
+                RoomReservation reservation = new RoomReservation(id,fname,lname,start,end,capacity,code);
+                RoomReservation updatedReservation = RoomReservationDao.updateInfo(reservation);
+                if(updatedReservation != null){
+                    out.println("Update was Successful ! ");
+                    out.println("New Information : ");
+                    out.println(updatedReservation.toString());
+                }
+            }catch (NullPointerException e){
+                out.println("An error occurred ! Please try again ...");
+                request.getRequestDispatcher("reservation.html").include(request,response);
+            }
         }
         out.println("</body></html>");
     }
