6223f64c22d659502889086bed49bc093f25e136
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-31 00:11:41 +0430

    CancellationServlet last version

--- src/main/java/com/hotel/Hotel_Reservation_final/controller/CancellationServlet.java
+++ src/main/java/com/hotel/Hotel_Reservation_final/controller/CancellationServlet.java
@@ -1,15 +1,35 @@
 package com.hotel.Hotel_Reservation_final.controller;
 
+import com.hotel.Hotel_Reservation_final.dao.RoomReservationDao;
+import com.hotel.Hotel_Reservation_final.model.RoomReservation;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "CancellationServlet")
 public class CancellationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String code = request.getParameter("code");
+        RoomReservation reservation = RoomReservationDao.showAllInfo(code);
+        if(reservation != null){
+            if(RoomReservationDao.cancel(code)){
+                out.println("Reservation canceled successfully !");
+            }
+            else{
+                out.println("Sorry ! An error has occurred . Please try again ...");
+                request.getRequestDispatcher("cancellation.html");
+            }
+        }
+        else{
+            out.println("Code not found !");
+        }
 
     }
 
