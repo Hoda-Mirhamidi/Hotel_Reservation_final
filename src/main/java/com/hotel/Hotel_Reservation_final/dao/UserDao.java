f7d505cc2428af9d622dbdf85067918210f59b0c
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-04-01 12:08:55 +0430

    signUp method added

--- src/main/java/com/hotel/Hotel_Reservation_final/dao/UserDao.java
+++ src/main/java/com/hotel/Hotel_Reservation_final/dao/UserDao.java
@@ -1,4 +1,28 @@
 package com.hotel.Hotel_Reservation_final.dao;
 
+import com.hotel.Hotel_Reservation_final.hibernate.HibernateUtil;
+import com.hotel.Hotel_Reservation_final.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class UserDao {
+
+    public static boolean signUp(User user){
+        Session session = HibernateUtil.sessionFactory.openSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return true;
+        }catch (HibernateException exception){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            return false;
+        }finally {
+            session.close();
+        }
+    }
 }
