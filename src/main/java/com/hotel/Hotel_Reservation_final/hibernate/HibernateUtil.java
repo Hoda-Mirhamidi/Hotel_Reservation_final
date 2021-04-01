92328b4495a85c34d050fbe0e39c89bb0fb34d21
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-04-01 11:36:52 +0430

    HibernateUtil class added

---
+++ src/main/java/com/hotel/Hotel_Reservation_final/hibernate/HibernateUtil.java
@@ -0,0 +1,16 @@
+package com.hotel.Hotel_Reservation_final.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+    public static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+}
